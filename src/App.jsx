423460ad391ef73877fb67356f2402f7f1cabe8c
Vitalii Maksymenko <maxymenkovitaly@gmail.com>
2024-07-15 00:58:33 +0200

    Prefer Computed Values & Avoid Unnecessary State

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState } from "react"; //
 import Player from "./components/Player";
-import Gameboard from "./components/GameBoard";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X"); //
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -24,11 +40,12 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <Gameboard
-          onSelectSquare={handleSelectSquare}
+        <GameBoard
+          onSelectSquare={handleSelectSquare} //
           activePlayerSymbol={activePlayer}
         />
       </div>
+      <Log />
     </main>
   );
 }
